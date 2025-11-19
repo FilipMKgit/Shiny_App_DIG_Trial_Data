8c48cc819c3f10abc26472fd9f84345eb2ecff6b
FilipMKgit <filipmikolajk@gmail.com>
2025-11-19 14:21:29 +0000

    first server

---
+++ server.R
@@ -0,0 +1,33 @@
+library(shiny)   # Web app development
+library(haven)   # Read in SAS dataset
+library(bslib)   # Provide UI options
+library(ggplot2) # Data visualization
+library(scales)  # Improve axis labels
+library(plotly)# Interactive graphics
+library(readr)
+library(dplyr)
+
+dig.df <- read_csv("DIG.csv")
+
+server <- function(input, output, session) {
+  
+  output$boxplot <- renderPlot({
+    ggplot(data = dig.df, aes(x = TRT01A, 
+                            y = .data[[input$subject_data]], 
+                            fill = TRT01A)) +
+      geom_boxplot() +
+      geom_jitter(width = 0.3, alpha = 0.4) +
+      theme_minimal() +
+      theme(legend.position = "none",
+            text = element_text(size = 15)) +
+      labs(
+        title = "ADSL Data",
+        subtitle = "Comparing Treatment Groups",
+        x = "",
+        y = attributes(adsl[[input$subject_data]])
+      ) +
+      scale_x_discrete(labels = label_wrap(10))
+  }, res = 100)
+}
+
+shinyApp(ui, server)
