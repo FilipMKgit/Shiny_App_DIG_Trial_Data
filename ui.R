f99174594467f330b4bf2d4d753c1e60c608f14b
tomdelany04 <tomdelany04@gmail.com>
2025-11-21 11:28:53 +0000

    adding comments, removing pre-selected radio buttons

--- ui.R
+++ ui.R
@@ -8,31 +8,30 @@ ui <- fluidPage(
     sidebarPanel(
       selectInput(
         "Variable", "Digitalis Data",
-        choices = c("Age" = "AGE",
-        "BMI" = "BMI",
-        "Creatine" = "CREAT",
-        "Diastolic BP" = "DIABP",
-        "Systolic BP" = "SYSBP")
-        ),
+         choices = c("Age" = "AGE",
+         "BMI" = "BMI",
+         "Creatine" = "CREAT",
+         "Diastolic BP" = "DIABP",
+         "Systolic BP" = "SYSBP")
+         ), # Variables to be displayed on boxplot and Density plot
    
-      radioButtons(
-      "dist_type",
-      "Distribution Type:",
-      choices = c("Density plot" = "density",
+       radioButtons( #These buttons allow user to choose density/histogram
+       "dist_type",
+       "Distribution Type:",
+       choices = c("Density plot" = "density",
                   "Histogram" = "hist"),
-      selected = "density"
-    )
-   ),
-    mainPanel(
-    card(
-      card_header("Boxplots for Baseline Variables"),
-      plotlyOutput("boxplot")
-     ),
-    card(
+     )
+    ),
+     mainPanel( #Main panel will consist of the boxplot as the top card and the density/histogram below
+     card(
+       card_header("Boxplots for Baseline Variables"),
+       plotlyOutput("boxplot")
+      ),
+      card(
       card_header("Distributions by treatment groups"),
       plotlyOutput("age_count_plot")
-    )
+     )
+   )
   )
-)
-)
+ )
 
