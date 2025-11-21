72e35106c52e6363543418403720c334e4ba228b
tomdelany04 <tomdelany04@gmail.com>
2025-11-21 11:22:16 +0000

    Adding radio buttons for density on UI

--- ui.R
+++ ui.R
@@ -6,20 +6,33 @@
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      selectInput("Variable", "Digitalis Data",
-                  choices = c("Age" = "AGE",
-                              "BMI" = "BMI",
-                              "Creatine" = "CREAT",
-                              "Diastolic BP" = "DIABP",
-                              "Systolic BP" = "SYSBP"))),
+      selectInput(
+        "Variable", "Digitalis Data",
+        choices = c("Age" = "AGE",
+        "BMI" = "BMI",
+        "Creatine" = "CREAT",
+        "Diastolic BP" = "DIABP",
+        "Systolic BP" = "SYSBP")
+        ),
+   
+      radioButtons(
+      "dist_type",
+      "Distribution Type:",
+      choices = c("Density plot" = "density",
+                  "Histogram" = "hist"),
+      selected = "density"
+    )
+   ),
     mainPanel(
-      plotlyOutput("boxplot"),
-      
-      br(),
-      h3("Number of patients by age"),
+    card(
+      card_header("Boxplots for Baseline Variables"),
+      plotlyOutput("boxplot")
+     ),
+    card(
+      card_header("Distributions by treatment groups"),
       plotlyOutput("age_count_plot")
     )
   )
 )
-
+)
 
